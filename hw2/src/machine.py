e5da5d757cff4be20f92d3540c8ca2d5a16609f8
Saba-SH <sshos20@freeuni.edu.ge>
2022-06-01 22:29:49 +0400

    add small comment

--- hw2/src/machine.py
+++ hw2/src/machine.py
@@ -418,7 +418,8 @@ class TwoTapeTuringMachine:
                 TM.add_transition(we_back_at_it_again + 13, TuringMachineTransition(BABAMBABAM, we_back_at_it_again + 14, BABAMBABAM, 'L'))
                 # it is over. or is it?
                 TM.add_transition(we_back_at_it_again + 14, TuringMachineTransition(BABAMBABAM, STATE_OFFSET + curr_target * STATES_PER_STATE, BABAMBABAM, 'L'))
-
+            
+            # this will be the accept state
             TM.add_state()
 
         ################################################################################
