5faf6a9238f18488cf56807b80fe07fbe337cc0e
Saba-SH <sshos20@freeuni.edu.ge>
2022-06-04 15:17:46 +0400

    complete testing script for first part

--- hw2/testp1.sh
+++ hw2/testp1.sh
@@ -18,33 +18,49 @@ SIM_PROG_COMMAND="python3 src/simulate.py"
 ################################################################
 for I in $(seq -f "%03g" $TEST_FIRST $TEST_LAST)
 do
+    printf "\n~~~~~~~~~~~TEST $I STARTED~~~~~~~~~~~~\n\n"
+
     INPUT_FILE="$INPUT_FOLDER/in$I.txt"
     TEST_FILE="$TEST_FOLDER/t$I.txt"
     ANSWER_FILE="$ANSWER_FOLDER/a$I.txt"
 
     CONVERTED_MACHINE="$(eval cat $INPUT_FILE | $CONV_PROG_COMMAND)"
-
-    INPUT_STRINGS=$(cat $INPUT_FILE)
-    ANSWERS=$(cat $ANSWER_FILE)
-    for J in 1..${#INPUT_STRINGS[@]}
+    
+    TEST_STRINGS=( )
+    while IFS= read -r line
     do
-        SIMULATION_OUTPUT=$(eval "echo "$'$CONVERTED_MACHINE\n${INPUT_STRINGS[J]}'" | $SIM_PROG_COMMAND")
-        TRIMMED_SIMULATION_OUTPUT=$(echo $SIMULATION_OUTPUT | xargs)
-        if [ "${TRIMMED_SIMULATION_OUTPUT:-2:2}" == "-1" ]
+        TEST_STRINGS+=( "$line" )
+    done <<< "$(cat $TEST_FILE)"
+    TEST_STRINGS_COUNT=${#TEST_STRINGS[@]}
+
+    readarray -t ANSWERS < $ANSWER_FILE
+    
+    for (( J = 0; J < $TEST_STRINGS_COUNT; J++ ))
+    do
+        PROG_INPUT="$(printf "$CONVERTED_MACHINE\n${TEST_STRINGS[$J]}")"
+        
+        TMPFILENAME="tmp${RANDOM}${RANDOM}${RANDOM}${RANDOM}_${I}_${J}"
+        echo "$PROG_INPUT" >$TMPFILENAME
+        SIMULATION_OUTPUT=$(eval "cat $TMPFILENAME | $SIM_PROG_COMMAND")
+        rm $TMPFILENAME
+        
+        TRIMMED_SIMULATION_OUTPUT="$(echo $SIMULATION_OUTPUT | xargs)"
+
+        if [ "${TRIMMED_SIMULATION_OUTPUT: -2: 2}" == "-1" ]
         then
-            if [ "${ANSWERS[J]}" == "T" ]
+            if [ "${ANSWERS[$J]}" == "T" ]
             then
-                echo "#########TEST $I FAILED####### ON STRING $J"
-                break
+                echo "#######TEST $I FAILED####### ON STRING $(($J + 1))"
+                # break
             fi
         else
-            if [ "${ANSWERS[J]}" == "F" ]
+            if [ "${ANSWERS[$J]}" == "F" ]
             then
-                echo "#########TEST $I FAILED####### ON STRING $J"
-                break
+                echo "#######TEST $I FAILED####### ON STRING $(($J + 1))"
+                # break
             fi
         fi
     done
-    echo "TEST $I COMPLETED"
+    printf "\n~~~~~~~~~~~TEST $I FINISHED~~~~~~~~~~~\n"
     
 done
